a73c530250b54a6dc2376e32d4c78069a363a124
vishwamihi <98463616+vishwamihi@users.noreply.github.com>
2024-08-05 20:59:02 +0530

    Update autoreact.js

--- src/plugin/autoreact.js
+++ src/plugin/autoreact.js
@@ -9,24 +9,24 @@ const autoreadCommand = async (m, Matrix) => {
   const text = m.body.slice(prefix.length + cmd.length).trim().toLowerCase();
 
   if (cmd === 'autoreact') {
-    if (!isCreator) return m.reply("*📛 THIS IS AN OWNER COMMAND*");
+    if (!isCreator) return m.reply("*💀 𝐎𝐍𝐋𝐘 𝐎𝐖𝐍𝐄𝐑𝐒*");
     let responseMessage;
 
     if (text === 'on') {
       config.AUTO_REACT = true;
-      responseMessage = "AUTO_REACT has been enabled.";
+      responseMessage = "𝙰𝚄𝚃𝙾 𝚁𝙴𝙰𝙲𝚃 𝙸𝚂 𝚆𝙾𝚁𝙺𝙸𝙽𝙶 ✅";
     } else if (text === 'off') {
       config.AUTO_REACT = false;
-      responseMessage = "AUTO_REACT has been disabled.";
+      responseMessage = "𝙰𝚄𝚃𝙾 𝚁𝙴𝙰𝙲𝚃 𝙸𝚂 𝙽𝙾𝚃 𝚆𝙾𝚁𝙺𝙸𝙽𝙶 ❌";
     } else {
-      responseMessage = "Usage:\n- `autoreact on`: Enable Auto-React\n- `autoreact off`: Disable Auto-React";
+      responseMessage = "Usage:\n- `autoreact on`: 𝙰𝚄𝚃𝙾 𝚁𝙴𝙰𝙲𝚃 𝙸𝚂 𝚆𝙾𝚁𝙺𝙸𝙽𝙶 ✅\n- `autoreact off`: 𝙰𝚄𝚃𝙾 𝚁𝙴𝙰𝙲𝚃 𝙸𝚂 𝙽𝙾𝚃 𝚆𝙾𝚁𝙺𝙸𝙽𝙶 ❌";
     }
 
     try {
       await Matrix.sendMessage(m.from, { text: responseMessage }, { quoted: m });
     } catch (error) {
       console.error("Error processing your request:", error);
-      await Matrix.sendMessage(m.from, { text: 'Error processing your request.' }, { quoted: m });
+      await Matrix.sendMessage(m.from, { text: '𝑬𝒓𝒓𝒐𝒓 𝒑𝒓𝒐𝒄𝒆𝒔𝒔𝒊𝒏𝒈 𝒚𝒐𝒖𝒓 𝒓𝒆𝒒𝒖𝒆𝒔𝒕.' }, { quoted: m });
     }
   }
 };
