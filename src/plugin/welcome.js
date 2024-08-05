7bb8bb90d6650c3cab606c39db82fa927f1a8ef1
vishwamihi <98463616+vishwamihi@users.noreply.github.com>
2024-08-05 21:04:39 +0530

    Update welcome.js

--- src/plugin/welcome.js
+++ src/plugin/welcome.js
@@ -7,32 +7,32 @@ const gcEvent = async (m, Matrix) => {
   const text = m.body.slice(prefix.length + cmd.length).trim().toLowerCase();
 
   if (cmd === 'welcome') {
-    if (!m.isGroup) return m.reply("*📛 THIS COMMAND CAN ONLY BE USED IN GROUPS*");
+    if (!m.isGroup) return m.reply("*𝙾𝙽𝙻𝚈 𝙾𝚆𝙽𝙴𝚁 𝙲𝙰𝙽 𝚄𝚂𝙴 𝙸𝙽 𝙶𝚁𝙾𝚄𝙿 ❗");
     const groupMetadata = await Matrix.groupMetadata(m.from);
     const participants = groupMetadata.participants;
     const botNumber = await Matrix.decodeJid(Matrix.user.id);
     const botAdmin = participants.find(p => p.id === botNumber)?.admin;
     const senderAdmin = participants.find(p => p.id === m.sender)?.admin;
 
-    if (!botAdmin) return m.reply("*📛 BOT MUST BE AN ADMIN TO USE THIS COMMAND*");
-    if (!senderAdmin) return m.reply("*📛 YOU MUST BE AN ADMIN TO USE THIS COMMAND*");
+    if (!botAdmin) return m.reply("*𝙱𝙾𝚃 𝙼𝚄𝚂𝚃 𝙱𝙴 𝙰𝙽 𝙰𝙳𝙼𝙸𝙽 𝚃𝙾 𝚄𝚂𝙴 𝚃𝙷𝙸𝚂 𝙲𝙾𝙼𝙼𝙰𝙽𝙳 ❗*");
+    if (!senderAdmin) return m.reply("*𝚈𝙾𝚄 𝙼𝚄𝚂𝚃 𝙱𝙴 𝙰𝙽 𝙰𝙳𝙼𝙸𝙽 𝚃𝙾 𝚄𝚂𝙴 𝚃𝙷𝙸𝚂 𝙲𝙾𝙼𝙼𝙰𝙽𝙳 ❗*");
     let responseMessage;
 
     if (text === 'on') {
       config.WELCOME = true;
-      responseMessage = "WELCOME & LEFT message has been enabled.";
+      responseMessage = "𝚆𝙴𝙻𝙲𝙾𝙼𝙴 & 𝙻𝙴𝙵𝚃 𝙸𝚂 𝚆𝙾𝚁𝙺𝙸𝙽𝙶 ✅";
     } else if (text === 'off') {
       config.WELCOME = false;
-      responseMessage = "WELCOME & LEFT message has been disabled.";
+      responseMessage = "𝚆𝙴𝙻𝙲𝙾𝙼𝙴 & 𝙻𝙴𝙵𝚃 𝙸𝚂 𝙽𝙾𝚃 𝚆𝙾𝚁𝙺𝙸𝙽𝙶 ❌";
     } else {
-      responseMessage = "Usage:\n- `WELCOME on`: Enable WELCOME & LEFT message\n- `WELCOME off`: Disable WELCOME & LEFT message";
+      responseMessage = "Usage:\n- `WELCOME on`: 𝚆𝙴𝙻𝙲𝙾𝙼𝙴 & 𝙻𝙴𝙵𝚃 𝙸𝚂 𝚆𝙾𝚁𝙺𝙸𝙽𝙶 ✅\n- `WELCOME off`: 𝚆𝙴𝙻𝙲𝙾𝙼𝙴 & 𝙻𝙴𝙵𝚃 𝙸𝚂 𝙽𝙾𝚃 𝚆𝙾𝚁𝙺𝙸𝙽𝙶 ❌";
     }
 
     try {
       await Matrix.sendMessage(m.from, { text: responseMessage }, { quoted: m });
     } catch (error) {
       console.error("Error processing your request:", error);
-      await Matrix.sendMessage(m.from, { text: 'Error processing your request.' }, { quoted: m });
+      await Matrix.sendMessage(m.from, { text: '𝑬𝒓𝒓𝒐𝒓 𝒑𝒓𝒐𝒄𝒆𝒔𝒔𝒊𝒏𝒈 𝒚𝒐𝒖𝒓 𝒓𝒆𝒒𝒖𝒆𝒔𝒕.' }, { quoted: m });
     }
   }
 };
